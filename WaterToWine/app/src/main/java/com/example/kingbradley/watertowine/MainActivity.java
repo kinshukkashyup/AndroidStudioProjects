54c194b1859b1e5eb41d99b60cfc3f9c77190d3c
kinshukkashyup <kinshukkashyup@gmail.com>
2018-06-19 19:22:25 -0400

    minor updates

--- WaterToWine/app/src/main/java/com/example/kingbradley/watertowine/MainActivity.java
+++ WaterToWine/app/src/main/java/com/example/kingbradley/watertowine/MainActivity.java
@@ -1,21 +1,18 @@
 package com.example.kingbradley.watertowine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.TextView;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     RadioButton rbtnFluid;
     RadioButton rbtnCup;
